2ed98542c4af000f0f174e7caed092ea14b25313
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-02-17 10:01:18 -0800

    Printing combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,26 +7,14 @@
 
 int main(void)
 {
-int i, j, k;
+int i;
 for (i = 0; i < 10; i++)
 {
-for (j = i; j < 10; j++)
-{
-for (k = j; k < 10; k++)
-{
 putchar(i + '0');
-putchar(',');
-putchar(' ');
-putchar(j + '0');
-putchar(',');
-putchar(' ');
-putchar(k + '0');
-if (i != 7 || j != 8 || k != 9)
+if (i < 9)
 {
 putchar(',');
 putchar(' ');
-}
-}
 }
 }
 putchar('\n');
