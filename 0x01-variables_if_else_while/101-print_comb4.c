078f7bd4a58bef090be08cb7111496ecbe704a48
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-02-17 10:26:14 -0800

    Commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,20 +8,20 @@
 int main(void)
 {
 int i, j, k;
-for (i = 0; i < 7; i++)
+for (i = 0; i <= 7; i++)
 {
-for (j = i + 1; j < 8; j++)
+for (j = i + 1; j <= 8; j++)
 {
-for (k = j + 1; k < 9; k++)
+for (k = j + 1; k <= 9; k++)
 {
 putchar(i + '0');
-putchar(',');
-putchar(' ');
 putchar(j + '0');
-putchar(',');
-putchar(' ');
 putchar(k + '0');
-if (i < 7 || j < 8 || k < 9)
+if (i == 7 && j == 8 && k == 9)
+{
+putchar('\n');
+}
+else
 {
 putchar(',');
 putchar(' ');
@@ -29,6 +29,5 @@ putchar(' ');
 }
 }
 }
-putchar('\n');
 return (0);
 }
