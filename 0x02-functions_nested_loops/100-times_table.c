138bd4344e8fab3543b715ec3f753fb5f8fb4c42
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-03-08 10:15:26 +0000

    Prints a times table starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,33 +7,33 @@
 */
 void print_times_table(int n)
 {
-  int i, j;
+int i, j;
 
-  if (n < 0 || n > 15)
+if (n < 0 || n > 15)
 	return;
 
-  for (i = 0; i <= n; i++)
-	{
-	  _putchar('0');
-	  for (j = 1; j <= n; j++)
-		{
-		  _putchar(',');
-		  _putchar(' ');
-		  if (i * j < 100)
-			_putchar(' ');
-		  if (i * j < 10)
-			_putchar(' ');
-		  if (i * j >= 100)
-			{
-			  _putchar((i * j) / 100 + '0');
-			  _putchar(((i * j) / 10) % 10 + '0');
-			}
-		  else if (i * j >= 10)
-			{
-			  _putchar((i * j) / 10 + '0');
-			}
-		  _putchar((i * j) % 10 + '0');
-		}
-	  _putchar('\n');
-	}
+for (i = 0; i <= n; i++)
+{
+	_putchar('0');
+for (j = 1; j <= n; j++)
+{
+	_putchar(',');
+	_putchar(' ');
+if (i * j < 100)
+_putchar(' ');
+if (i * j < 10)
+_putchar(' ');
+if (i * j >= 100)
+{
+_putchar((i * j) / 100 + '0');
+_putchar(((i * j) / 10) % 10 + '0');
+
+else if (i * j >= 10)
+{
+_putchar((i * j) / 10 + '0');
+}
+_putchar((i * j) % 10 + '0');
+}
+_putchar('\n');
+}
 }
