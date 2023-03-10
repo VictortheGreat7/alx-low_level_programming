b24fdcdf1696c35a1326a5a49c51e7a6954a8687
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-03-10 16:38:12 +0000

    A program that prints its own name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -18,3 +18,27 @@ _puts(argv[i]);
 
 return (0);
 }
+
+/**
+ * _puts - prints a string to stdout
+ *
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+  int i;
+
+  for (i = 0; str[i] != '\0'; i++)
+	{
+	  _putchar(str[i]);
+	}
+
+  _putchar('\n');
+}
+
+#include <unistd.h>
+
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
