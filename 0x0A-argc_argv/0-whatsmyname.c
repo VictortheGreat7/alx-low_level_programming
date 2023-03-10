5eb3aa677bb49fe5252f9f0d811bed0f54bdf21b
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-03-10 18:26:20 +0000

    A program that prints its own name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,16 +10,7 @@
 */
 int main(__attribute__((unused)) int argc, char *argv[])
 {
-char *program_name;
-
-program_name = strrchr(argv[0], '/');
-
-if (program_name == NULL)
-program_name = argv[0];
-else
-program_name++;
-
-printf("%s\n", program_name);
+printf("%s\n", argv[0]);
 
 return (0);
 }
