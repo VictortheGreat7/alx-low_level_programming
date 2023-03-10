2c371a6c338f95756a88b1ecb6dc056c8d0e014a
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-03-10 19:37:52 +0000

    Indicates if a string is a palindrome or not

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,22 +7,34 @@
 */
 int is_palindrome(char *s)
 {
-char *p = s;
-int len = 0;
-
-while (*p != '\0')
-{
-p++;
-len++;
+int len = _strlen_recursion(s);
+return (is_palindrome_helper(s, 0, len - 1));
 }
 
-while (s < p)
+/**
+* is_palindrome_helper - helper function to check if a string is a palindrome
+* @s: pointer to the string
+* @start: starting index of the string
+* @end: ending index of the string
+* Return: 1 if the string is a palindrome, 0 otherwise
+*/
+int is_palindrome_helper(char *s, int start, int end)
 {
-if (*s != *(p - 1))
-return (0);
-s++;
-p--;
-}
-
+if (start >= end)
 return (1);
+if (s[start] != s[end])
+return (0);
+return (is_palindrome_helper(s, start + 1, end - 1));
+}
+
+/**
+* _strlen_recursion - Computes the length of a string recursively
+* @s: pointer to the string
+* Return: the length of the string
+*/
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+return (0);
+return (1 + _strlen_recursion(s + 1));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define NULL ((void *)0)
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -11,5 +13,6 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int is_palindrome_helper(char *s, int start, int end);
 
 #endif
