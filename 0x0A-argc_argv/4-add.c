e958dd6de7ac14973480140ccf52e8a0523fc093
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-03-10 19:08:18 +0000

    Adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,16 +9,16 @@
 */
 int is_positive_number(char *str)
 {
-  int i = 0;
+int i = 0;
 
-  while (str[i] != '\0')
-	{
-	  if (str[i] < '0' || str[i] > '9')
-		return (0);
-	  i++;
-	}
+while (str[i] != '\0')
+{
+if (str[i] < '0' || str[i] > '9')
+return (0);
+i++;
+}
 
-  return (1);
+return (1);
 }
 
 /**
