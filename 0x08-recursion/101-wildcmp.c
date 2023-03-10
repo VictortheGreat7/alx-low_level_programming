1259cdf07c1f244031e98c08381e6e5e8f717427
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-03-10 19:53:49 +0000

    Indicates if two strings are identical or not

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,30 +13,17 @@ if (*s1 == '\0')
 {
 if (*s2 == '\0')
 return (1);
-else if (*s2 == '*')
+if (*s2 == '*')
 return (wildcmp(s1, s2 + 1));
-else
 return (0);
 }
-else if (*s2 == '\0')
+if (*s2 == '*')
 {
-return (0);
-}
-else if (*s1 == *s2)
-{
-return (wildcmp(s1 + 1, s2 + 1));
-}
-else if (*s2 == '*')
-{
-if (*(s2 + 1) == '\0')
+if (wildcmp(s1, s2 + 1))
 return (1);
-else if (*s1 == *(s2 + 1))
-return (wildcmp(s1 + 1, s2 + 2) || wildcmp(s1 + 1, s2));
-else
-return (wildcmp(s1, s2 + 1));
+return (wildcmp(s1 + 1, s2));
 }
-else
-{
+if (*s1 == *s2)
+return (wildcmp(s1 + 1, s2 + 1));
 return (0);
-}
 }
