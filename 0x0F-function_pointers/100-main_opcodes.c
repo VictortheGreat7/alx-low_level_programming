2d87f86a6cbbd7e8e49b766de2c31c3d920b1af4
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-03-22 17:11:47 +0000

    Opcodes program

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,39 +2,38 @@
 #include <stdlib.h>
 
 /**
- * main - prints the opcodes of its own main function
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: 0 on success, 1 on incorrect argument count, 2 on negative byte count
- */
+* main - prints the opcodes of its own main function
+* @argc: argument count
+* @argv: argument vector
+* Return: 0 on success, 1 on incorrect argument count, 2 on negative byte count
+*/
 int main(int argc, char *argv[])
 {
-  int i, bytes;
+int i, bytes;
 
-  if (argc != 2)
-	{
-	  printf("Error\n");
-	  return 1;
-	}
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
 
-  bytes = atoi(argv[1]);
+bytes = atoi(argv[1]);
 
-  if (bytes < 0)
-	{
-	  printf("Error\n");
-	  return 2;
-	}
+if (bytes < 0)
+{
+printf("Error\n");
+return (2);
+}
 
-  char *addr = (char *)main;
+char *addr = (char *)main;
 
-  for (i = 0; i < bytes; i++)
-	{
-	  printf("%02hhx", addr[i]);
-	  if (i != bytes - 1)
-		printf(" ");
-	}
+for (i = 0; i < bytes; i++)
+{
+printf("%02hhx", addr[i]);
+if (i != bytes - 1)
+printf(" ");
+}
 
-  printf("\n");
-  return 0;
+printf("\n");
+return (0);
 }
