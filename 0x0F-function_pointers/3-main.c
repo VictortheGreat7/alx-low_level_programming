4525e98c224d418c656191c3bdbf4b6b99b918bf
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-03-22 17:00:52 +0000

    Calculator program

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,42 +1,42 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
+#include <stdio.h>
 
 /**
-* main - performs simple operations
-* @argc: number of arguments
-* @argv: array of arguments
-* Return: 0 if successful, otherwise 98, 99, or 100
+* main - entry point for the program
+* @argc: the number of command line arguments
+* @argv: an array of strings containing the command line arguments
+* Return: 0 on success, otherwise 98, 99, or 100 on failure
 */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 int num1, num2, result;
-int (*op_func)(int, int);
+int (*func)(int, int);
 
 if (argc != 4)
 {
-printf("Error\n");
-return (98);
+puts("Error");
+exit(98);
 }
 
 num1 = atoi(argv[1]);
 num2 = atoi(argv[3]);
 
-op_func = get_op_func(argv[2]);
+func = get_op_func(argv[2]);
 
-if (!op_func)
+if (func == NULL)
 {
-printf("Error\n");
-return (99);
+puts("Error");
+exit(99);
 }
 
 if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 {
-printf("Error\n");
-return (100);
+puts("Error");
+exit(100);
 }
 
-result = op_func(num1, num2);
+result = func(num1, num2);
+
 printf("%d\n", result);
 
 return (0);
