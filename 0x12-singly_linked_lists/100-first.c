659c128cb6a3cf1df24d9e4cdee62b82559e3d0f
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-03-30 11:16:28 +0000

    5. The Hare and the Tortoise: Betty Fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+* before_main - function that prints a message before main is executed
+*
+* Return: void
+*/
 void __attribute__((constructor)) before_main()
 {
 printf("You're beat! and yet, you must allow,\n"
