1bcf8d5a44bac57f09b19f795b997730006bbb64
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-04-04 10:34:21 +0000

    Task 4: Fix 1

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,8 +3,6 @@
 /**
 * free_listint - Frees a listint_t list.
 * @head: Pointer to the start of the list.
-* @temp: Pointer to a place that temporarily stores the current node being
-* freed.
 * Return: Nothing.
 */
 void free_listint(listint_t *head)
@@ -14,7 +12,7 @@ listint_t *temp;
 while (head != NULL)
 {
 temp = head;
-head =head->next;
+head = head->next;
 free(temp);
 }
 }
