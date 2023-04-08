3c3be1888badfd376358a98e63f94db9b257458a
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-04-08 14:43:25 +0000

    Tasks 0 - 4: Check 1

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* clear_bit - Sets the value of a bit to 0 at a given index
+* @n: Pointer to the number to be modified
+* @index: The index, starting from 0, of the bit to set
+* Return: 1 if it worked, or -1 if an error occurred
+* Description: The bitmask variable is used to pinpoint the bit to be set
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+unsigned long int bitmask;
+bitmask = 1 << index;
+bitmask = ~(1 << index);
+*n &= bitmask;
+
+if (index >= sizeof(unsigned long int) * 8)
+{
+return (-1);
+}
+return (1);
+}
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+int count = 0;
+
+}
