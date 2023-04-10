7cb0ee565afb97481027ee2761b300880562835e
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-04-10 16:20:22 +0000

    Task 0: Edit and Task 1: Check 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,10 +14,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 int fd  = open(filename, O_RDONLY);
 char *buffer = malloc(letters);
-ssize_t n_read, n_written;
+ssize_t bytes_read, bytes_written;
 
-n_read = read(fd, buffer, letters);
-n_written = write(STDOUT_FILENO, buffer, n_read);
+bytes_read = read(fd, buffer, letters);
+bytes_written = write(STDOUT_FILENO, buffer, n_read);
 
 if (filename == NULL)
 return (0);
@@ -30,14 +30,14 @@ close(fd);
 return (0);
 }
 
-if (n_read == -1)
+if (bytes_read == -1)
 {
 free(buffer);
 close(fd);
 return (0);
 }
 
-if (n_written == -1 || n_written != n_read)
+if (bytes_written == -1 || bytes_written != n_read)
 {
 free(buffer);
 close(fd);
@@ -45,5 +45,5 @@ return (0);
 }
 free(buffer);
 close(fd);
-return (n_written);
+return (bytes_written);
 }
