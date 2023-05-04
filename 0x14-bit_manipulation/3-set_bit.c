ffe7e33570fdd6d98880943619f2c1ae85e43ec5
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-05-04 12:00:10 +0000

    Task 3: Check 1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* set_bit - Sets the value of a bit to 1 at a given index
+* @n: Pointer to the number to modify
+* @index: The index, starting from 0, of the bit to set
+* Return: 1 if it worked, or -1 if an error occurred
+* Description: The bitmask variable is used to pinpoint the bit to be set
+*/
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+unsigned long int bitmask;
+bitmask = 1 << index;
+*n |= bitmask;
+if (index >= sizeof(unsigned long int) * 8)
+return (-1);
+*n |= 1UL << index;
+return (1);
+}
