1e7caa76818e5b1048b7b861391b9c2d22af796d
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-05-04 12:31:20 +0000

    Task 6: Check 1

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* get_endianness - Chexks the endianness of the system
+* get_endianness - Checks the endianness of the system
 * Return: 0 if big endian, 1 if little endian
 */
 int get_endianness(void)
