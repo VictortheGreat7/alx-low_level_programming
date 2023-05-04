12e67c71263493108f51e3a13cf5671ccd4a80a8
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-05-04 11:40:28 +0000

    Task 1: Check 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_binary - Prints the binary representation of n
+* @n: The number meant to be converted
+* Description: The bitmask variable is used to pinpoint the desired bit
+* Return: Nothing
+*/
+
+void print_binary(unsigned long int n)
+{
+unsigned long int bitmask = 1;
+int shift = 0;
+while (bitmask <= n >> 1)
+{
+shift += 1;
+bitmask = bitmask << 1;
+}
+while (shift >= 0)
+{
+_putchar(((n >> shift) &1) + '0');
+shift--;
+}
+}
