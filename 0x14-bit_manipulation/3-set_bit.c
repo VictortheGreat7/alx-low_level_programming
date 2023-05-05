42f806b47d44332379a0f3e0468855ac99a8ff03
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-05-05 07:29:44 +0000

    Task 3: Check 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,11 +9,10 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-unsigned long int bitmask;
-bitmask = 1 << index;
-*n |= bitmask;
+unsigned long int bitmask = 1UL << index;
+
 if (index >= sizeof(unsigned long int) * 8)
 return (-1);
-*n |= 1UL << index;
+*n |= bitmask;
 return (1);
 }
