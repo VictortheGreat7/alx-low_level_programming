ae539fbcbbee03bdb4a8654d3e1cfec5cc6876d9
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-05-08 14:03:23 +0000

    File I/O: Task 1: Check 8

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,9 @@
 
 int create_file(const char *filename, char *text_content)
 {
-int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+int flags = O_WRONLY | O_CREAT | O_TRUNC;
+int mode = S_IRUSR | S_IWUSR;
+int fd = open(filename, flags, mode);
 int len = 0;
 ssize_t bytes_written = write(fd, text_content, len);
 
@@ -23,13 +25,11 @@ return (-1);
 
 if (text_content != NULL)
 {
-while (text_content[len] != '\0')
-len++;
-}
 if (bytes_written == -1 || bytes_written != (ssize_t) len)
 {
 close(fd);
 return (-1);
+}
 }
 close(fd);
 return (1);
