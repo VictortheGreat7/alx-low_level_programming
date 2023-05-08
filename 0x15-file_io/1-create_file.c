20d69cfae24e2acc977177c5dea32c24dc6b1301
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-05-08 17:04:30 +0000

    Task 1: Back to first attempt

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,8 +15,8 @@ int create_file(const char *filename, char *text_content)
 int flags = O_WRONLY | O_CREAT | O_TRUNC;
 int mode = S_IRUSR | S_IWUSR;
 int fd = open(filename, flags, mode);
-int len = 0;
-int bytes_written = write(fd, text_content, len);
+size_t len = strlen(text_content);
+ssize_t bytes_written = write(fd, text_content, len);
 
 if (filename == NULL)
 return (-1);
@@ -25,12 +25,11 @@ return (-1);
 
 if (text_content != NULL)
 {
-if (bytes_written == -1 || bytes_written != len)
+if (bytes_written == -1 || bytes_written != (ssize_t) len)
 {
 close(fd);
 return (-1);
 }
-len++;
 }
 close(fd);
 return (1);
