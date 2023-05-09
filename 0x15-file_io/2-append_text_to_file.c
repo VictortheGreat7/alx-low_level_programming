e232ef19a036021d77d2d0e0f9b3679ba3e4e2ad
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-05-09 10:09:55 +0000

    Task 2: Check 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,8 +10,8 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 int fd = open(filename, O_WRONLY | O_APPEND);
-size_t text_length = strlen(text_content);
-ssize_t bytes_written = write(fd, text_content, text_length);
+size_t text_length;
+ssize_t bytes_written;
 
 if (filename == NULL)
 return (-1);
@@ -20,11 +20,18 @@ return (-1);
 
 if (text_content != NULL)
 {
+text_length = strlen(text_content);
+bytes_written = write(fd, text_content, text_length);
 if (bytes_written != (ssize_t) text_length)
 {
 close(fd);
 return (-1);
 }
+}
+else
+{
+text_length = 0;
+bytes_written = 0;
 }
 close(fd);
 return (1);
