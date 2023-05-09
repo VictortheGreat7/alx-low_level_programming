0c8710058dc27d1a3984b766771dc8933f438a04
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-05-09 08:24:25 +0000

    Task 1: Check 17

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,17 +14,17 @@ int create_file(const char *filename, char *text_content)
 {
 int mode = S_IRUSR | S_IWUSR;
 int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
-size_t len = strlen(text_content);
+size_t len = 0;
 ssize_t bytes_written = write(fd, text_content, len);
 
 if (filename == NULL)
 return (-1);
 if (fd == -1)
 return (-1);
-len = 0;
 if (text_content != NULL)
-{
 len = strlen(text_content);
+if (len > 0)
+{
 if (bytes_written == -1 || bytes_written != (ssize_t) len)
 {
 close(fd);
